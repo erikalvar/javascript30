fefd64a38e60165b818fdbb87f3376c9197ae29e
erikalvar <erikalvarbass@gmail.com>
2021-02-01 20:58:06 -0500

    adds play, update, skip, and scrub js

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -15,7 +15,7 @@ function togglePlay() {
 
 function updateButton() {
   const icon = this.paused ? "►" : "❚ ❚";
-  console.log(icon);
+  // console.log(icon);
   toggle.textContent = icon;
 }
 
@@ -32,6 +32,11 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /* Hook up the event listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -45,8 +50,8 @@ ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
 
-// let mousedown = false;
-// progress.addEventListener("click", scrub);
-// progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
-// progress.addEventListener("mousedown", () => (mousedown = true));
-// progress.addEventListener("mouseup", () => (mousedown = false));
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
